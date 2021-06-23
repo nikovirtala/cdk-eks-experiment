b10919092420cc9d1d1a84c3957837d8cbcb193e
Niko Virtala <niko.virtala@hey.com>
2021-06-23 20:07:18 +0300

    chore: upgrade projen, cdk and all deps.

--- .projenrc.js
+++ .projenrc.js
@@ -1,7 +1,7 @@
 const { AwsCdkTypeScriptApp } = require('projen');
 
 const project = new AwsCdkTypeScriptApp({
-  cdkVersion: '1.107.0',
+  cdkVersion: '1.109.0',
   defaultReleaseBranch: 'main',
   jsiiFqn: 'projen.AwsCdkTypeScriptApp',
   name: 'cdk-eks-experiment',
