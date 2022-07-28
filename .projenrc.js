451e082d62fe6e4c6ead815f2ccef70c26a4b6dc
Niko Virtala <nikovirtala@users.noreply.github.com>
2022-07-28 14:15:06 +0300

    chore(deps): upgrade dependencies (#371)
    Upgrades project dependencies. See details in [workflow run].
    
    [Workflow Run]: https://github.com/nikovirtala/cdk-eks-experiment/actions/runs/2750594287
    
    ------
    
    *Automatically created by projen via the "upgrade" workflow*

--- .projenrc.js
+++ .projenrc.js
@@ -1,7 +1,7 @@
 const { awscdk } = require('projen');
 
 const project = new awscdk.AwsCdkTypeScriptApp({
-  cdkVersion: '2.17.0',
+  cdkVersion: '2.33.0',
   defaultReleaseBranch: 'main',
   jsiiFqn: 'projen.AwsCdkTypeScriptApp',
   name: 'cdk-eks-experiment',
